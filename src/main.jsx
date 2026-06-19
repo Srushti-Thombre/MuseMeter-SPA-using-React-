cac32d8a474afe7b25e06fbfee448362eee30477
Boddapalli Leelavardhan <leelavardhan9@gmail.com>
2026-06-19 09:39:47 +0000

    feat: migrating from create react app to vite for optiming production and dev server builds

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root')); 
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+); 
+
 root.render( 
-<BrowserRouter>
- <App /> 
- </BrowserRouter> 
+    <React.StrictMode>
+        <BrowserRouter>
+            <App /> 
+        </BrowserRouter> 
+    </React.StrictMode>
 );
-
-
-
